c597e8e5ecd84d1352b8684d8c605b32abb54a08
bportela <bportela.dev@gmail.com>
2023-03-16 22:25:07 +0100

    plantilla de proyecto

---
+++ test/Beporsoft.TabularSheet.Test/UnitTestTabularData.cs
@@ -0,0 +1,16 @@
+namespace Beporsoft.TabularSheet.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Create()
+        {
+            TabularData<int> table = new TabularData<int>();
+        }
+    }
+}
