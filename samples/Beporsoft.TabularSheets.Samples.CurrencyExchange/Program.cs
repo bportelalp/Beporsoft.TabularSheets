360a1b267c16cfb8cc664e7010ed2a3f8c6c42a5
bportelalp <bportelalopez@outlook.es>
2023-06-09 19:19:49 +0200

    Migrate sample net48 from legacy project to sdk-style project

--- samples/Beporsoft.TabularSheets.Samples.CurrencyExchange/Program.cs
+++ samples/Beporsoft.TabularSheets.Samples.CurrencyExchange/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Net.Http;
-using System.Reflection;
 
 namespace Beporsoft.TabularSheets.Samples.CurrencyExchange
 {
