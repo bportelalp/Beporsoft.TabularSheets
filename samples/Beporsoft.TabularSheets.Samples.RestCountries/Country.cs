9bbb2b3351333660b25f1778165b1897be894ca5
bportelalp <bportelalopez@outlook.es>
2023-09-29 16:29:22 +0200

    Sample Csv on rest countries

--- samples/Beporsoft.TabularSheets.Samples.RestCountries/Country.cs
+++ samples/Beporsoft.TabularSheets.Samples.RestCountries/Country.cs
@@ -4,31 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-#pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
-
 namespace Beporsoft.TabularSheets.Samples.RestCountries
 {
 
     public class Country
     {
-        public Name Name { get; set; }
-        public List<string> Capital { get; set; }
-        public Dictionary<string, Currency> Currencies { get; set; }
-        public string Region { get; set; }
-        public Dictionary<string, string> Languages { get; set; }
+        public Name Name { get; set; } = null!;
+        public List<string> Capital { get; set; } = null!;
+        public Dictionary<string, Currency> Currencies { get; set; } = null!;
+        public string Region { get; set; } = null!;
+        public Dictionary<string, string> Languages { get; set; } = null!;
         public int Population { get; set; }
     }
 
     public class Name
     {
-        public string Common { get; set; }
-        public string Official { get; set; }
+        public string Common { get; set; } = null!;
+        public string Official { get; set; } = null!;
     }
 
     public class Currency
     {
-        public string Name { get; set; }
-        public string Symbol { get; set; }
+        public string Name { get; set; } = null!;
+        public string Symbol { get; set; } = null!;
     }
 
 }
